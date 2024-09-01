9e3953851c5ce02aa34ad9084102e11ddf781384
Nikolay A <hake1488@gmail.com>
2024-09-01 19:26:56 +0300

    Initial commit

---
+++ website/settings_local.py
@@ -0,0 +1,16 @@
+
+
+import os
+
+from website.settings import BASE_DIR
+
+DEBUG = True
+
+SECRET_KEY = 'b0e@^m&tccz11$w59qov$lhn-97!(%wfn-gray-c*x)^a$wx=2'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
